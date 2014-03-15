bf0cc7890b0ba8dcdd0f165779d81260abbf84f2
Dave Goerlich <dgoerlich@designinginteractive.com>
2014-03-15 12:13:18 -0400

    The basic blink LED script

---
+++ blink.rb
@@ -0,0 +1,14 @@
+require ‘pi_piper’
+include PiPiper
+
+led_pin = PiPiper::Pin.new(pin: 18, direction: :out)
+
+led_pin.off
+
+0.step(2) do
+  led_pin.on
+  sleep 0.5
+  led_pin.off
+  sleep 0.5
+end
+
